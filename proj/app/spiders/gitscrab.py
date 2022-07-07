8825d418e93925e38f1eb00a195f1cda5f865a5d
Omoegod <omoegod@icloud.com>
2022-07-07 15:09:47 +0300

    v2

--- proj/app/spiders/gitscrab.py
+++ proj/app/spiders/gitscrab.py
@@ -1,85 +1,41 @@
-import collections
-from posixpath import split
-import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 from app.items import Repositories
-from urllib.parse import urljoin
 
-from pymongo import MongoClient
 
-client = MongoClient()
-db = client.gitscrab
-collections = db.collections
-
-class GitscrabSpider(scrapy.Spider):
+class GitscrabSpider(CrawlSpider):
     name = 'gitscrab'
-    start_urls = None
+    allowed_domains = ['github.com']
+    start_urls = ['https://github.com/scrapy'] #None
 
-    def start_requests(self):
-        if not GitscrabSpider.start_urls:
-            url = [item for item in input("Enter URL(one url or list [url url url...] ): ").split()]
-            GitscrabSpider.start_urls = url
+    rules = [
+        Rule(LinkExtractor(restrict_xpaths=('//*[@id="js-pjax-container"]/div/header/div[2]/nav/div/ul/li[2]/a'),), follow=True),
+        Rule(LinkExtractor(restrict_xpaths=('//*[@id="org-repositories"]/div/div/div[1]/ul/li/div/div[1]/div[1]/h3/a'),), callback='parse', follow=True),
+    ]
 
-        print("URLs: " + str(GitscrabSpider.start_urls))
-        for url in GitscrabSpider.start_urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
-    def parse(self, response):
-
-        for repos_list_link in response.xpath(
-           '//*[@id="js-pjax-container"]/div/header/div[2]/nav/div/ul/li[2]/a/@href').getall():
-            url = urljoin(response.url, repos_list_link)
-            #print(url)
-            yield response.follow(url)
-            
-            for repos_link in response.xpath('//*[@id="org-repositories"]/div/div/div[1]/ul/li/div/div[1]/div[1]/h3/a/@href').getall():
-                url = urljoin(response.url, repos_link)
-                #print(url)
-                yield response.follow(url, callback = self.parse_repo)  
-                
-    def parse_repo(self, response):
-
+    def parse(self, response): 
+        
         item = Repositories()
-        rep_name = str(response.xpath('//*[@id="repository-container-header"]/div[1]/div/h1/strong/a/text()').get()).strip()    
-        item['rep_name'] = rep_name
-        description = str(response.xpath('//*[@id="repo-content-pjax-container"]/div/div[3]/div[2]/div/div[1]/div/p/text()').get()).strip()
-        item['description'] = description
-        url = response.xpath('//*[@id="repo-content-pjax-container"]/div/div[3]/div[2]/div/div[1]/div/div[1]/span/a/@href').get()
-        item['url'] = url
-        stars = response.xpath('//*[@id="repo-content-pjax-container"]/div/div[3]/div[2]/div/div[1]/div/div[6]/a/strong/text()').get()
-        item['stars'] = stars
-        forks = response.xpath('//*[@id="repo-content-pjax-container"]/div/div[3]/div[2]/div/div[1]/div/div[8]/a/strong/text()').get()
-        item['forks'] = forks
-        watching = response.xpath('//*[@id="repo-content-pjax-container"]/div/div[3]/div[2]/div/div[1]/div/div[7]/a/strong/text()').get()
-        item['watching'] = watching
-        commits = response.xpath('//*[@id="repo-content-pjax-container"]/div/div[3]/div[1]/div[2]/div[1]/div/div[4]/ul/li/a/span/strong/text()').get()
-        item['commits'] = commits
-        author_commit =  response.xpath('//*[@id="repo-content-pjax-container"]/div/div[3]/div[1]/div[2]/div[1]/div/div[2]/div[1]/a/text()').get()
-        name_commit = response.xpath('//*[@id="repo-content-pjax-container"]/div/div[3]/div[1]/div[2]/div[1]/div/div[2]/div[1]/span/a[1]/text()').get()
-        datetime_commit = response.xpath('//*[@id="repo-content-pjax-container"]/div/div[3]/div[1]/div[2]/div[1]/div/div[2]/div[2]/a[2]/relative-time/@datetime').get()
-        item['info_commit'] = author_commit, name_commit, datetime_commit
-        releases = response.xpath('//*[@id="repo-content-pjax-container"]/div/div[3]/div[2]/div/div[2]/div/h2/a/span/text()').get()
-        item['releases'] = releases
+        item['rep_url'] = response.url
+        item['username'] = response.xpath('//*[@id="repository-container-header"]/div[1]/div/div/span[1]/a/text()').get()
+        item['rep_name'] = response.xpath('//*[@id="repository-container-header"]/div[1]/div/div/strong/a/text()').get()
+        item['description'] = " ".join(response.xpath('//*[@id="repo-content-pjax-container"]/div/div/div[3]/div[2]/div/div[1]/div/p//text()').getall()).replace("\n","").strip()
+        item['stars'] = response.xpath('//*[@id="repo-stars-counter-star"]/@title').get()
+        item['forks'] = response.xpath('//*[@id="repo-network-counter"]/@title').get()
+        item['watching'] = response.xpath('//*[@id="repo-content-pjax-container"]/div/div/div[3]/div[2]/div/div[1]/div/div[7]/a/strong/text()').get()
+        item['url'] = response.xpath('//*[@id="repo-content-pjax-container"]/div/div/div[3]/div[2]/div/div[1]/div/div[1]/span/a/@href').get()
+        item['commits'] = response.xpath('//*[@id="repo-content-pjax-container"]/div/div/div[3]/div[1]/div[2]/div[1]/div/div[4]/ul/li/a/span/strong/text()').get()
+        item['author_commit'] =  response.xpath('//*[@id="repo-content-pjax-container"]/div/div/div[3]/div[1]/div[2]/div[1]/div/div[2]/div[1]/a/text()').get()
+        item['name_commit'] = ''.join(str(response.xpath('//*[@id="repo-content-pjax-container"]/div/div/div[3]/div[1]/div[2]/div[1]/div/div[2]/div[1]/span/a[1]/@title').get()).split('\n'))
+        item['datetime_commit'] = response.xpath('//*[@id="repo-content-pjax-container"]/div/div/div[3]/div[1]/div[2]/div[1]/div/div[2]/div[2]/a[2]/relative-time/@datetime').get()
+        item['releases'] = response.xpath('//*[@id="repo-content-pjax-container"]/div/div/div[3]/div[2]/div/div[2]/div/h2/a/span/text()').get()
+        item['version_releases'] = response.xpath('//*[@id="repo-content-pjax-container"]/div/div/div[3]/div[2]/div/div[2]/div/a/div/div[1]/span[1]/text()').get()
+        item['datetime_releases'] = response.xpath('//*[@id="repo-content-pjax-container"]/div/div/div[3]/div[2]/div/div[2]/div/a/div/div[2]/relative-time/@datetime').get()
+        url_relaise = response.xpath('//*[@id="repo-content-pjax-container"]/div/div/div[3]/div[2]/div/div[2]/div/h2/a/@href').get()  
+        return response.follow(url_relaise, self.parse_changlog, cb_kwargs=dict(item=item))
+
+    def parse_changlog(self, response, item):
         
-        def parse(response):
-            
-            version_releases = str(response.xpath('//*[@id="repo-content-pjax-container"]/div[2]/div[1]/div[2]/div/div[1]/div[2]/div[1]/h1/a/text()').get()).strip()
-            datetime_releases = response.xpath('//*[@id="repo-content-pjax-container"]/div[2]/div[1]/div[1]/div[1]/local-time/@datetime').get()   
-            changelog_releases = response.xpath('//*[@id="repo-content-pjax-container"]/div[2]/div[1]/div[2]/div/div[1]/div[4]//text()').getall()
-            changelog_releases = " ".join(changelog_releases).replace("\n","")
-            item['info_releases'] = version_releases, datetime_releases, changelog_releases 
-            yield item
-
-        collections.insert_one(dict(item))
-
-        for url_releases in response.xpath('//*[@id="repo-content-pjax-container"]/div/div[3]/div[2]/div/div[2]/div/h2/a/@href').getall():
-            url = urljoin(response.url, url_releases)
-            #print(url)
-            yield response.follow(url, callback = parse)    
-           
-   
+        item['changelog_releases'] = " ".join(response.xpath('//*[@id="repo-content-pjax-container"]/div/div[2]/div[1]/div[2]/div/div[1]/div[4]//text()').getall()).replace("\n","")
+        yield item 
         
-    
-
-
-
-
